b0717b909c79fa2d04d21d20208ddf10c2a510a5
inabatk <totemo.sunda.aoi.sora@gmail.com>
2013-12-14 11:52:55 -0500

    Added SSL samples.

--- mqtt/src/main/java/hoge/exp/mqtt/SampleMqttPublisher.java
+++ mqtt/src/main/java/hoge/exp/mqtt/SampleMqttPublisher.java
@@ -6,32 +6,32 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class SampleMqttPublisher {
-public static void main(String[] args) {
-	String uri = "tcp://localhost:1883";
-	String clientId = "publisher01";
-	String topic = "topic01";
-	String message = "hello";
+	public static void main(String[] args) {
+		String uri = "tcp://localhost:1883";
+		String clientId = "publisher01";
+		String topic = "topic01";
+		String message = "hello";
 
-	MqttClient client = null;
-	try {
-			client = new MqttClient(uri, clientId, new MemoryPersistence());
+		MqttClient client = null;
+		try {
+				client = new MqttClient(uri, clientId, new MemoryPersistence());
 
-			client.connect();
+				client.connect();
 
-			client.publish(topic, new MqttMessage(message.getBytes()));
-		} catch (MqttException e) {
-			e.printStackTrace();
-		} finally {
-			if (client != null) {
-				try {
-					if (client.isConnected()) {
-						client.disconnect();
+				client.publish(topic, new MqttMessage(message.getBytes()));
+			} catch (MqttException e) {
+				e.printStackTrace();
+			} finally {
+				if (client != null) {
+					try {
+						if (client.isConnected()) {
+							client.disconnect();
+						}
+						client.close();
+					} catch (MqttException e) {
+						e.printStackTrace();
 					}
-					client.close();
-				} catch (MqttException e) {
-					e.printStackTrace();
 				}
 			}
-		}
-}
+	}
 }
