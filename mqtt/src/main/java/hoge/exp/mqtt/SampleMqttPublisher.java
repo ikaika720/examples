41756461c787a9ccff7075a1a4a170fc19c1c518
inabatk <totemo.sunda.aoi.sora@gmail.com>
2013-12-14 10:25:45 -0500

    Added simple mqtt clients.

---
+++ mqtt/src/main/java/hoge/exp/mqtt/SampleMqttPublisher.java
@@ -0,0 +1,35 @@
+package hoge.exp.mqtt;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class SampleMqttPublisher {
+public static void main(String[] args) {
+	String uri = "tcp://localhost:1883";
+	String clientId = "publisher01";
+	String topic = "topic01";
+	String message = "hello";
+
+	MqttClient client = null;
+	try {
+			client = new MqttClient(uri, clientId, new MemoryPersistence());
+
+			client.connect();
+
+			client.publish(topic, new MqttMessage(message.getBytes()));
+		} catch (MqttException e) {
+			e.printStackTrace();
+		} finally {
+			if (client != null) {
+				try {
+					client.disconnect();
+					client.close();
+				} catch (MqttException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+}
+}
---
+++ mqtt/src/main/java/hoge/exp/mqtt/SampleMqttSubscriber.java
@@ -0,0 +1,57 @@
+package hoge.exp.mqtt;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class SampleMqttSubscriber implements MqttCallback {
+	public static void main(String[] args) {
+		String uri = "tcp://localhost:1883";
+		String clientId = "subscriber01";
+		String topic = "topic01";
+
+		MqttClient client = null;
+		try {
+			client = new MqttClient(uri, clientId, new MemoryPersistence());
+
+			client.connect();
+
+			client.setCallback(new SampleMqttSubscriber());
+			client.subscribe(topic);
+
+			System.out.println("Press any key to exit.");
+			try {System.in.read();} catch (Exception e) {}
+		} catch (MqttException e) {
+			e.printStackTrace();
+		} finally {
+			if (client != null) {
+				try {
+					client.disconnect();
+					client.close();
+				} catch (MqttException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void connectionLost(Throwable cause) {
+		System.out.println("connectionLost");
+		cause.printStackTrace();
+	}
+
+	@Override
+	public void messageArrived(String topic, MqttMessage message)
+			throws Exception {
+		System.out.println(new String(message.getPayload()));
+	}
+
+	@Override
+	public void deliveryComplete(IMqttDeliveryToken token) {
+		System.out.println("deliveryComplete");
+	}
+}
