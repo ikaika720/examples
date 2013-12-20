7f01029f052d45b84648fa50108015bb455951d7
inabatk <totemo.sunda.aoi.sora@gmail.com>
2013-12-19 19:01:30 -0500

    Added an example of named query and pagination.

---
+++ jpa-standalone/src/main/java/hoge/exp/jpa_standalone/Member3.java
@@ -0,0 +1,61 @@
+package hoge.exp.jpa_standalone;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "member")
+@NamedQuery(name = "Member3.findAll", query = "SELECT m FROM Member3 m")
+public class Member3 {
+    @Id
+    private long id;
+    private String name;
+    private String email;
+    private Date dateOfBirth;
+
+    public Member3() {}
+
+    public Member3(long id, String name, String email, Date dateOfBirth) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Member [id=" + id +
+                ", name=\"" + name +
+                "\", email=\"" + email +
+                "\", deteOfBirth=\"" + dateOfBirth + "\"]";
+    }
+}
---
+++ jpa-standalone/src/main/java/hoge/exp/jpa_standalone/NamedQueryAndPagination.java
@@ -0,0 +1,84 @@
+package hoge.exp.jpa_standalone;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class NamedQueryAndPagination {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-standalonPU");
+
+        try {
+            EntityManager em = emf.createEntityManager();
+            EntityTransaction transaction = em.getTransaction();
+
+            // delete
+            {
+                transaction.begin();
+                em.createNativeQuery("TRUNCATE TABLE member").executeUpdate();
+                transaction.commit();
+            }
+
+            // create
+            {
+                int numOfRecords = 95;
+
+                String memberFormat = "member%05d";
+                String emailFormat = "member%05d@email.com";
+
+                transaction.begin();
+
+                for (int i = 0; i < numOfRecords; i++) {
+                    Member3 member = new Member3(i, String.format(memberFormat, i),
+                            String.format(emailFormat, i), new Date());
+                    em.persist(member);
+                }
+
+                transaction.commit();
+            }
+
+            // read
+            {
+                int pageSize = 10;
+                for (int i = 0;; i++) {
+                    transaction.begin();
+
+                    TypedQuery<Member3> query =
+                            em.createNamedQuery("Member3.findAll", Member3.class)
+                                .setMaxResults(pageSize)
+                                .setFirstResult(i * pageSize);
+                    List<Member3> result = query.getResultList();
+
+                    if (result.size() == 0) {
+                        transaction.commit();
+                        break;
+                    }
+
+                    System.out.println("Page " + i);
+
+                    for (Member3 m : result) {
+                        System.out.println(m);
+                    }
+
+                    transaction.commit();
+                }
+            }
+
+            // delete
+            {
+                transaction.begin();
+                em.createNativeQuery("TRUNCATE TABLE member").executeUpdate();
+                transaction.commit();
+            }
+        } finally {
+            if (emf != null) {
+                emf.close();
+            }
+        }
+    }
+}
