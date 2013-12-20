f085939b6873e2bd9a684c949dedb05301e139f9
inabatk <totemo.sunda.aoi.sora@gmail.com>
2013-12-20 18:32:52 -0500

    Added a JAX-RS 2.0 example.

---
+++ jaxrs/README.md
@@ -0,0 +1,9 @@
+JAX-RS 2.0 examples
+===================
+
+The root of resource classes is "/". See src/main/webapp/WEB-INF/web.xml.
+
+Access http://localhost:8080/jaxrs/hello for `Hello`.
+
+For further information
+ * [Building RESTful Web Services with JAX-RS](http://docs.oracle.com/javaee/7/tutorial/doc/jaxrs.htm)
---
+++ jaxrs/src/main/java/hoge/exp/jaxrs/Hello.java
@@ -0,0 +1,15 @@
+package hoge.exp.jaxrs;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("hello")
+public class Hello {
+    @GET
+    @Produces("text/html")
+    public String getHtml() {
+        return "<html><head><title>hello</title><body><h1>hello</h1></body></html>";
+    }
+
+}
