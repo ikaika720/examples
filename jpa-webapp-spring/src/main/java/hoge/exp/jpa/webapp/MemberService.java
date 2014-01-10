1b314e83ff2134e485b13388ea6d2d23aab1383b
inabatk <totemo.sunda.aoi.sora@gmail.com>
2014-01-10 16:01:55 -0500

    Added an example of JPA using Spring

---
+++ jpa-webapp-spring/src/main/java/hoge/exp/jpa/webapp/MemberService.java
@@ -0,0 +1,17 @@
+package hoge.exp.jpa.webapp;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    @PersistenceContext(unitName = "jpa-webappPU")
+    private EntityManager em;
+
+    public Member getById(long id) {
+        return em.find(Member.class, id);
+    }
+}
