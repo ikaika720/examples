0d346fda08d98fb1d265ca98be0726a5032f9764
inabatk <totemo.sunda.aoi.sora@gmail.com>
2014-01-13 21:13:38 -0500

    Added transaction config.

--- jpa-webapp-spring-tomcat/src/main/java/hoge/exp/jpa/webapp/MemberService.java
+++ jpa-webapp-spring-tomcat/src/main/java/hoge/exp/jpa/webapp/MemberService.java
@@ -4,12 +4,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class MemberService {
     @PersistenceContext(unitName = "jpa-webappPU")
     private EntityManager em;
 
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Member getById(long id) {
         return em.find(Member.class, id);
     }
