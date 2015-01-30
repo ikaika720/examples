26b05a96356fa7d5a1f3583b4430b5fa2b31a083
inabatk <inabatk@users.noreply.github.com>
2015-01-30 09:30:22 -0500

    version up

--- docker/integration/maven/build.sh
+++ docker/integration/maven/build.sh
@@ -5,7 +5,7 @@ dbip=172.24.0.11
 tar xzf /shared/jpa-webapp-spring.tar.gz
 cd jpa-webapp-spring
 sed -i -e 's/\(<jta-data-source>\).*\(<\/jta-data-source>\)/\1java:jboss\/datasources\/MyDB\2/' src/main/resources/META-INF/persistence.xml
-mvn package -DskipTests=true
+mvn package
 cp target/jpa-webapp-spring*.war /shared
 
 driver=$(find /root/.m2/repository/org/postgresql/postgresql -name 'postgresql-*jdbc*.jar' -type f)
