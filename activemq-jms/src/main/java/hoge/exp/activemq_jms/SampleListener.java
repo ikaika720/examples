2aec87069cd86d9704de33f3bac7926e21085cfa
ikaika720 <ikaika720@users.noreply.github.com>
2025-08-10 21:10:24 +0900

    use lambda

--- activemq-jms/src/main/java/hoge/exp/activemq_jms/SampleListener.java
+++ activemq-jms/src/main/java/hoge/exp/activemq_jms/SampleListener.java
@@ -5,48 +5,42 @@ import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 import jakarta.jms.Connection;
 import jakarta.jms.Destination;
 import jakarta.jms.JMSException;
-import jakarta.jms.Message;
 import jakarta.jms.MessageConsumer;
-import jakarta.jms.MessageListener;
 import jakarta.jms.Session;
 import jakarta.jms.TextMessage;
 
-public class SampleListener implements MessageListener {
+public class SampleListener {
 	public static void main(String[] args) {
 		String brokerURL = "tcp://localhost:61616";
 		String username = "artemis";
 		String password = "artemis";
 		String queueName = "queue01";
-		long wait_time = 30_000;
 
-        try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(brokerURL);
-             Connection conn = cf.createConnection(username, password)) {
+		try (ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory(brokerURL);
+				Connection conn = cf.createConnection(username, password)) {
 			Session session = conn.createSession(false,
 					Session.AUTO_ACKNOWLEDGE);
 
 			Destination dest = session.createQueue(queueName);
 
 			MessageConsumer consumer = session.createConsumer(dest);
-			consumer.setMessageListener(new SampleListener());
+			consumer.setMessageListener(message -> {
+				try {
+					TextMessage textMessage = (TextMessage) message;
+					System.out.println("Message: " + textMessage.getText() +
+							", Correlation ID: " + textMessage.getJMSCorrelationID());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			});
 
 			conn.start();
 
-			System.out.printf("wait %s ms\n", wait_time);
-			Thread.sleep(wait_time);
+    		System.out.println("Message handler started. Press Ctrl+C to stop.");
+    		Thread.currentThread().join();
 		} catch (JMSException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public void onMessage(Message message) {
-		try {
-			TextMessage textMessage = (TextMessage) message;
-			System.out.println("Message: " + textMessage.getText() + 
-				", Correlation ID: " + textMessage.getJMSCorrelationID());
-		} catch (JMSException e) {
 			e.printStackTrace();
 		}
 	}
